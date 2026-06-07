8dea53abb3e5cf027fe477f5cb040eddbcbc06c7
ademirtr54-beep <ademirtr54@gmail.com>
2026-06-06 23:30:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+console.log("BOT INICIOU");
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Bot online: ${client.user.tag}`);
+});
+
 client.on("messageCreate", (message) => {
   if (message.author.bot) return;
 
@@ -5,6 +21,5 @@ client.on("messageCreate", (message) => {
     message.reply("Pong 🏓");
   }
 });
-
 
 client.login(process.env.TOKEN);
